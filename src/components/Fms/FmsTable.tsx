3bf2118d293e1f14896feb58f58b8151b886e208
ruskov03 <irina.maximciuc@polymtl.ca>
2025-06-17 15:48:43 -0400

    ajustement du fms

--- src/components/Fms/FmsTable.tsx
+++ src/components/Fms/FmsTable.tsx
@@ -17,14 +17,23 @@ type Props = {
 };
 
 export default function FmsTable({ route }: Props) {
+  const noRoute = route.length === 0;
+
   return (
     <div className="bg-black text-white font-mono w-full h-full flex flex-col p-4">
       <FmsHeader />
       <FmsTableHeader />
+
       <div className="flex-1 overflow-y-auto mt-1 text-sm text-white/90">
-        {route.map((fix, i) => (
-          <FmsRow key={`${fix.fix}-${i}`} fix={fix} />
-        ))}
+        {noRoute ? (
+          <div className="text-center text-white/50 mt-10 text-base italic">
+            No flight plan available. Please Logon...
+          </div>
+        ) : (
+          route.map((fix, i) => (
+            <FmsRow key={`${fix.fix}-${i}`} fix={fix} />
+          ))
+        )}
       </div>
     </div>
   );
