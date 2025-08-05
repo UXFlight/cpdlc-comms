27185d3fa27b8314ff592fae48ace2da943e9692
ruskov03 <irina.maximciuc@polymtl.ca>
2025-08-05 00:17:36 -0400

    Fix static export with output: 'export'

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-// next.config.js
-const path = require('path');
+const path = require('path')
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, './src'),
-    };
-    return config;
-  },
-};
-module.exports = nextConfig;   
+  output: 'export',
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
+    return config
+  }
+}
+
+module.exports = nextConfig
