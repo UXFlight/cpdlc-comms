f867ae2908b398e37930a3f6b62a66c6df2ec8a0
simy46 <missipsa.annane@polymtl.ca>
2026-03-08 23:28:33 +0100

    fix: update start script in package.json and refine PROD_SERVER_URL handling

--- src/constants/serverURL.ts
+++ src/constants/serverURL.ts
@@ -1,5 +1,7 @@
 const DEV_SERVER_URL = "http://127.0.0.1:5321/";
-const PROD_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL ?? "";
+const PROD_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
 
 export const SERVER_URL =
-  process.env.NODE_ENV === "production" ? PROD_SERVER_URL : DEV_SERVER_URL;
+  process.env.NODE_ENV === "production"
+    ? PROD_SERVER_URL!
+    : DEV_SERVER_URL;
